72cf3b0eb59a4c00b2d03bcc418452407dd227a8
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2025-11-13 15:33:11 -0300

    feat:implementa-boot-termina-atividade

--- script.js
+++ script.js
@@ -1,54 +1,36 @@
 const draggables = document.querySelectorAll('.card');
 const dropzones = document.querySelectorAll('.card-drop');
 
-
-
-
 draggables.forEach(draggable => {
     draggable.addEventListener('dragstart', event => {
-        event.dataTransfer.setData('text/plain', event.target.innerText);
         event.target.classList.add('dragging');
-        console.log('drag iniciado: ', event.target.innerText);
     });
 
     draggable.addEventListener('dragend', event => {
-        event.target.classList.remove('draggin');
-        console.log('Drag finalizado: ', event.target.innerText);
+        event.target.classList.remove('dragging');
     });
 });
 
 dropzones.forEach(dropzone => {
-    dropzone.addEventListener('dragenter', event => {
+    dropzone.addEventListener('dragover', event => {
         event.preventDefault();
         dropzone.classList.add('over');
-        console.log('Entrou na area de drop');
-
-        dropzone.addEventListener('dragover', event => {
-            event.preventDefault();
-            console.log("Elemento sobre a area de drop");
-        });
-
-        dropzone.addEventListener('dragleave', () => {
-            dropzone.classList.remove('over');
-            console.log('Saiu da area de drop');
-        })
-
-        dropzone.addEventListener('drop', event => {
-            event.preventDefault();
-            dropzone.classList.remove('over');
-
-    const dragging = document.querySelector('.dragging'); 
-            if (dragging) {
-      dropzone.innerHTML = ''; 
-                dropzone.appendChild(dragging);
-                console.log('Elemento solto na área de drop');
-            }
-        });
-
     });
 
+    dropzone.addEventListener('dragleave', () => {
+        dropzone.classList.remove('over');
+    });
 
+    dropzone.addEventListener('drop', event => {
+        event.preventDefault();
+        dropzone.classList.remove('over');
 
+        const dragging = document.querySelector('.dragging');
+        if (dragging) {
+            dropzone.innerHTML = '';
+            const clone = dragging.cloneNode(true);
+            clone.classList.remove('dragging');
+            dropzone.appendChild(clone);
+        }
+    });
 });
-
-
